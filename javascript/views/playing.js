5a2990aba32c8d8b215c69f503cc9ca246b5cb0f
Brennan Falkner <Brennan@bfalkner.com>
2012-03-08 13:25:24 -0500

    Indentation.

--- javascript/views/playing.js
+++ javascript/views/playing.js
@@ -36,46 +36,48 @@ PlayingSC.addState("challenge", {
 
 /* (Session) */
 var PlayingView = Backbone.View.extend({
-    initialize: function() {
-        _.bindAll(this);
-        this.session = this.options.session;
-        this.template = Handlebars.compile($("#playing-template").html());
-    },
+  initialize: function() {
+    _.bindAll(this);
+    this.session = this.options.session;
+    this.template = Handlebars.compile($("#playing-template").html());
+  },
 
-    render: function() {
-        var data = {
-            title: this.session.get("movie").get("title"),
-            progress: this.session.get("movie").get("challenges").map(this._challengeData),
-            points: this.session.points()
-        };
-        this.$el.html(this.template(data));
-        return this;
-    },
+  render: function() {
+    var data = {
+      title: this.session.get("movie").get("title"),
+      progress: this.session.get("movie").get("challenges").map(this._challengeData),
+      points: this.session.points()
+    };
+    this.$el.html(this.template(data));
+    return this;
+  },
 
-    _challengeData: function(challenge) {
-        var isAnswered = this.session.isAnswered(challenge);
-        var isActive = this.session.isActive(challenge);
+  _challengeData: function(challenge) {
+    var isAnswered = this.session.isAnswered(challenge);
+    var isActive = this.session.isActive(challenge);
 
-        return {
-            class: "progressItem" + (isAnswered ? "-answered" : "") + (isActive ? "-active" : "")
-        };
-    },
+    return {
+      class: "progressItem"
+        + (isAnswered ? "-answered" : "")
+        + (isActive ? "-active" : "")
+    };
+  },
 
-    /* Events */
+  /* Events */
 
-    events: {
-        "click #playing-link-home": "home",
-        "click #playing-link-play": "play",
-        "click #playing-link-pause": "pause",
-        "click #challenge": "challenge"
-    },
+  events: {
+    "click #playing-link-home": "home",
+    "click #playing-link-play": "play",
+    "click #playing-link-pause": "pause",
+    "click #challenge": "challenge"
+  },
 
-    home: function() { PlayingSC.sendEvent("home"); },
-    play: function() { PlayingSC.sendEvent("play"); },
-    pause: function() { PlayingSC.sendEvent("pause"); },
-    challenge: function(e) {
-      var challenge = this.session.get("movie").get("challenges").first();
-      PlayingSC.sendEvent("hasChallenge", challenge);
-      e.preventDefault();
-    }
+  home: function() { PlayingSC.sendEvent("home"); },
+  play: function() { PlayingSC.sendEvent("play"); },
+  pause: function() { PlayingSC.sendEvent("pause"); },
+  challenge: function(e) {
+    var challenge = this.session.get("movie").get("challenges").first();
+    PlayingSC.sendEvent("hasChallenge", challenge);
+    e.preventDefault();
+  }
 });
